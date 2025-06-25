3140241bd4efb318a442fabef9a4441a7fcd016b
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-06-25 23:01:28 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://expense-tracker-nhnf.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
