f470b835fbc79fa6ab493526058838a03ce62f2b
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-06-26 02:09:13 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -7,7 +7,7 @@ dbConnect();
 const cors = require("cors");
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://et-manager.netlify.app"],
+    origin: "*",
     credentials: true,
   })
 );
