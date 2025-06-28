2cf16dbe13d3bb696ae6d6c9ef607a41549b4e94
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-06-28 15:18:24 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://crm-dashboard-k9ao.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
